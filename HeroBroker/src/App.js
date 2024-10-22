fd2d55577df8bab8752b0d17c463cd53b55c985e
Thomas R McNutt <pachown@gmail.com>
2024-10-22 02:58:35 -0600

    Finished item creation function
    Finished item creation function
    Added testing text to React to test item creation

--- HeroBroker/src/App.js
+++ HeroBroker/src/App.js
@@ -1,20 +1,46 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { generateItem } from "./functions/items";
 
 function App() {
+  const [items, setItems] = useState({});
+  // const [heroes, setHeroes] = useState([])
+  // const [missions, setMissions] = useState([])
 
-  const [items, setItems] = useState([])
-  const [heroes, setHeroes] = useState([])
-  const [missions, setMissions] = useState([])
+  useEffect(() => {
+    createItem(10, 0);
+  }, []);
+
+  function createItem(difficulty, itemFind) {
+    let newItems = generateItem(difficulty, itemFind);
+    setItems((items) => ({
+      ...items,
+      ...newItems,
+    }));
+
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="header">Header</div>
+      <header className="header">
+        <div className="header">Mod Type: {items.itemType}</div>
       </header>
       <body className="body">
-        <div>Body</div>
-        <div className="container"></div>
+        <div>Number of mods: {items.modifiers}</div>
+        {Array.isArray(items.mods) && items.mods.length > 0 ? (
+          items.mods.map((mod, index) => (
+            <div key={index}>
+              <p>
+                <strong>Mod Name:</strong> {mod.modName}
+              </p>
+              <p>
+                <strong>Mod Value:</strong> {mod.modValue}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>no mods</p>
+        )}
       </body>
       <footer className="footer">
         <div>Footer</div>
--- HeroBroker/src/functions/items.js
+++ HeroBroker/src/functions/items.js
@@ -1,66 +1,156 @@
 //generate items based on difficulty modifier and item find
 
-export function generateItem(difficulty, itemFind){
-    const itemTypes = [helmet, body, boot, glove, shoulder, ring, pants, belt, amulet, backpack, bracelet, sock, underwear, undershirt, cloak, glasses];
-    const itemMultipliers = {
-        helmet: 5,
-        body: 10,
-        boot: 3,
-        glove: 3,
-        shoulder: 3,
-        ring: 2,
-        pants: 8,
-        belt: 5,
-        amulet: 8,
-        backpack: 5,
-        bracelet: 2,
-        sock: 1,
-        underwear: 2,
-        undershirt: 2,
-        cloak: 4,
-        glasses: 3,
-    }
-    const powerStatBase = [1,2];
-    const weakStatBase = [1,5];
-    const stats = {
-        "strength" : weakStatBase,
-        "strength%" : powerStatBase,
-        "intelligence" : weakStatBase,
-        "intelligence%" : powerStatBase,
-        "agility" : weakStatBase,
-        "agility%" : powerStatBase,
-        "allStats" : weakStatBase,
-        "allStats%" : powerStatBase,
-        "health" : [10, 100],
-        "health%": powerStatBase,
-        "armor": weakStatBase,
-        "armor%": powerStatBase,
-        "damage": weakStatBase,
-        "damage%": powerStatBase,
-        "critChance": powerStatBase,
-        "critDamage": powerStatBase,
-        "attackSpeed": powerStatBase,
-        "thorns": weakStatBase,
-        "damageResist": powerStatBase,
-        "allResist": powerStatBase,
-        "fireResist": weakStatBase,
-        "physicalResist": weakStatBase,
-        "coldResist": weakStatBase,
-        "lightningResist": weakStatBase,
-        "psychicResist": weakStatBase,
-        "poisonResist": weakStatBase,
-        "chanceToBleed": weakStatBase,
-        "chanceToPoison": weakStatBase,
-        "chanceToBurn": weakStatBase,
-        "chanceToElectrocute": weakStatBase,
-        "chanceToFreeze": weakStatBase,
-        "chanceToStun": weakStatBase,
-        "chanceToExecute": weakStatBase,
-        "lifeRegeneration": powerStatBase,
-        "lifeOnKill": weakStatBase,
-        "lifeOnHit": powerStatBase,
-        "itemDrop%": [1,20],
-        "goldDrop%": [1,20],
-        "inventorySpace": [1,1],
-    }
+export function generateItem(difficulty, itemFind) {
+  //get a random item type from itemTypes
+  //roll a number of stats by getting a range from diffficultyToMods and randomly finding how many mods with the findRandom function
+
+  var itemTypes = [
+    "helmet",
+    "body",
+    "boot",
+    "glove",
+    "shoulder",
+    "ring",
+    "pants",
+    "belt",
+    "amulet",
+    "backpack",
+    "bracelet",
+    "sock",
+    "underwear",
+    "undershirt",
+    "cloak",
+    "glasses",
+  ];
+  var difficultyToMods = {
+    1: [0, 2],
+    2: [0, 3],
+    3: [1, 3],
+    4: [1, 4],
+    5: [2, 4],
+    6: [2, 5],
+    7: [3, 5],
+    8: [3, 6],
+    9: [4, 6],
+    10: [6, 8],
+  };
+
+  let item = {
+    itemType: itemTypes[findRandomWhole(itemTypes.length)],
+    modifiers: findRandomWholeRange(difficultyToMods[difficulty]),
+    mods: [],
+  };
+  for (let i = 0; i < item.modifiers; i++) {
+    let newMod = addStat(item.itemType);
+    item.mods.push(newMod);
+  }
+  item.mods = sumModValues(item.mods)
+
+  return item;
+}
+
+function addStat(itemType) {
+  var powerStatBase = [1, 2];
+  var weakStatBase = [1, 5];
+  var mods = {
+    strength: weakStatBase,
+    "strength%": powerStatBase,
+    intelligence: weakStatBase,
+    "intelligence%": powerStatBase,
+    agility: weakStatBase,
+    "agility%": powerStatBase,
+    allStats: weakStatBase,
+    "allStats%": powerStatBase,
+    health: [10, 100],
+    "health%": powerStatBase,
+    armor: weakStatBase,
+    "armor%": powerStatBase,
+    damage: weakStatBase,
+    "damage%": powerStatBase,
+    critChance: powerStatBase,
+    critDamage: powerStatBase,
+    attackSpeed: powerStatBase,
+    thorns: weakStatBase,
+    damageResist: powerStatBase,
+    allResist: powerStatBase,
+    fireResist: weakStatBase,
+    physicalResist: weakStatBase,
+    coldResist: weakStatBase,
+    lightningResist: weakStatBase,
+    psychicResist: weakStatBase,
+    poisonResist: weakStatBase,
+    chanceToBleed: weakStatBase,
+    chanceToPoison: weakStatBase,
+    chanceToBurn: weakStatBase,
+    chanceToElectrocute: weakStatBase,
+    chanceToFreeze: weakStatBase,
+    chanceToStun: weakStatBase,
+    chanceToExecute: weakStatBase,
+    lifeRegeneration: powerStatBase,
+    lifeOnKill: weakStatBase,
+    lifeOnHit: powerStatBase,
+    // "itemDrop%": [1, 20],
+    // "goldDrop%": [1, 20],
+    // inventorySpace: [1, 1],
+  };
+  var itemMultipliers = {
+    helmet: 5,
+    body: 10,
+    boot: 3,
+    glove: 3,
+    shoulder: 3,
+    ring: 2,
+    pants: 8,
+    amulet: 8,
+    backpack: 5,
+    bracelet: 2,
+    sock: 1,
+    underwear: 2,
+    undershirt: 2,
+    cloak: 4,
+    glasses: 3,
+    belt: 4
+  };
+
+  let keys = Object.keys(mods);
+  let modName = keys[findRandomWhole(keys.length)];
+  let modValue = findRandomTwoDecimal(mods[modName]) * itemMultipliers[itemType];
+
+  let newStat = {
+    modName: modName,
+    modValue: modValue.toFixed(2),
+  };
+  return newStat;
+}
+
+function findRandomTwoDecimal(array) {
+  return (Math.random() * (array[1] - array[0]) + array[0]);
+}
+
+function findRandomWholeRange(array) {
+  return Math.floor(Math.random() * (array[1] - array[0]) + array[0]);
+}
+
+function findRandomWhole(num) {
+  return Math.floor(Math.random() * num);
+}
+
+function sumModValues(modArray){
+    const modSums = {};
+
+    modArray.forEach(mod => {
+        const modName = mod.modName;
+        const modValue = parseFloat(mod.modValue);
+
+        if (modSums[modName]) {
+            modSums[modName] += modValue;
+            console.log("summed " + modName)
+        } else {
+            modSums[modName] = modValue;
+        }
+    });
+    return Object.entries(modSums).map(([modName, modValue]) => ({
+        modName,
+        modValue: modValue.toFixed(2) // Optional: rounding to two decimal places
+    }));
 }
